526724f86bde862e90de06e7dcf320573758352c
Reggie <75982750+gn01763009@users.noreply.github.com>
2022-09-15 10:44:23 +0800

    DL-21 Adding theme provider

---
+++ src/Theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const Theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0052cc',
+    },
+    secondary: {
+      main: '#edf2ff',
+    },
+  },
+});
+
+export default Theme
